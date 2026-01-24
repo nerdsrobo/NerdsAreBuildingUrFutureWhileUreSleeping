43c3a0245537bc69bbd411fdbc1c18e687d33e0b
POAL <pbraginets48@gmail.com>
2026-01-24 13:13:11 +0300

    imu update

--- modules/imu/ImuV1.kt
+++ modules/imu/ImuV1.kt
@@ -1,45 +1,34 @@
 package org.firstinspires.ftc.teamcode.modules.imu
 
-import com.qualcomm.hardware.bosch.BNO055IMU
-import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator
-import org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.telemetry
+import com.qualcomm.hardware.bosch.BHI260IMU
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot
+import com.qualcomm.robotcore.hardware.IMU
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit
 import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference
-import org.firstinspires.ftc.robotcore.external.navigation.Orientation
 import org.firstinspires.ftc.teamcode.modules.superclasses.Module
 import org.firstinspires.ftc.teamcode.modules.superclasses.RobotPack
 
 
 class ImuV1(P: RobotPack) : Module(P) {
-    var imu: BNO055IMU = P.hwmp.get(BNO055IMU::class.java, "imu");
+    var imu: BHI260IMU = P.hwmp.get(BHI260IMU ::class.java, "imu");
     init {
-        val parameters = BNO055IMU.Parameters() // инициализация Акселерометра
-        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES
-        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC
-        parameters.calibrationDataFile = "BNO055IMUCalibration.json"
-        parameters.loggingEnabled = true
-        parameters.loggingTag = "IMU"
-        parameters.accelerationIntegrationAlgorithm = JustLoggingAccelerationIntegrator()
-        imu = P.hwmp.get(BNO055IMU::class.java, "imu")
-        imu.initialize(parameters)
-        while (!imu.isGyroCalibrated) { //Калибровка акселерометра
-            telemetry.addData("Wait", "Calibration") //Сообщение о калибровке
-            telemetry.update()
-        }
-        telemetry.addData("Done!", "Calibrated") //Сообщение об окончании калибровки
-        telemetry.update()
+        imu = P.hwmp.get(BHI260IMU::class.java, "imu")
+
+        imu.initialize(
+            IMU.Parameters(
+                RevHubOrientationOnRobot(
+                    RevHubOrientationOnRobot.LogoFacingDirection.LEFT,
+                    RevHubOrientationOnRobot.UsbFacingDirection.DOWN
+                )
+            )
+        )
+        imu.resetYaw()
     }
 
     fun getAngle(): Float {
-        //Функция получения данных с акселерометра
-        val angles: Orientation =
-            imu.getAngularOrientation(
-                AxesReference.INTRINSIC,
-                AxesOrder.ZYX,
-                AngleUnit.DEGREES
-            ) // переменная в которой будет храниться угол поворота под акселерометр
-        val gravity = imu.gravity // здесь хранится важная информация для акселерометра
-        return angles.firstAngle
+        val orientation =
+            imu.getRobotOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES)
+        return -orientation.firstAngle
     }
 }
